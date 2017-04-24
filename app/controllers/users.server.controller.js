0875a86f9ce274d97d82a4e873dab390d61505e4
정성진 <oriongod8749@gmail.com>
2017-04-24 22:39:10 +0900

    CRUD 동작 추가(findOne, findByIdAndUpdate, remove) y

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -1,27 +1,64 @@
 const mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise;
-
 const User = mongoose.model('User');
 
 exports.create = (req, res, next) => {
-  console.log(req.body);
+  let user = new User(req.body);
 
   user.save((err) => {
     if(err) {
       return next(err);
     } else {
       res.json(user);
-    };
+    }
   });
 };
 
-exports.list = (req, res, next)=>{
+exports.list = (req, res, next) => {
   User.find((err, users)=>{
     if(err){
       return next(err);
     } else {
       res.json(users);
-    };
+    }
   });
-}
+};
+
+exports.read = (req, res) => {
+  if(req.user === null) {
+    return res.json({"err":"null!!!!"});
+  } else {
+    res.json(req.user);
+  }
+};
+
+exports.userById = (req, res, next, id) => {
+  User.findOne({_id:id}, (err, user) => {
+    if(err){
+      return next(err);
+    } else {
+      req.user = user;
+      next();
+    }
+  });
+};
+
+exports.update = (req, res, next) => {
+  User.findByIdAndUpdate(req.user.id, req.body, (err, user) => {
+    if(err){
+      return next(err);
+    } else {
+      res.json(user);
+    }
+  });
+};
+
+exports.delete = (req, res, next) => {
+  req.user.remove((err) => {
+    if(err){
+      return next(err);
+    } else {
+      res.json(req.user);
+    }
+  });
+};
