7f7a5af38723d8c2499e3487716f4e91285a156d
정성진 <oriongod8749@gmail.com>
2017-04-25 00:43:44 +0900

    스키마 확장, get/set/virtual 적용

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -3,9 +3,37 @@ const mongoose = require('mongoose'),
 
 const UserSchema = new Schema({
   userName: String,
-  userId: String,
+  userId: {
+    type: String,
+    trim: true
+  },
   password: String,
-  email: String
+  email: String,
+  webSite: {
+    type: String,
+    get: (url) => {
+      if(!url){
+        return url;
+      } else {
+        if(url.indexOf('http://') !== 0 && url.indexOf('https://') !== 0) {
+          url = 'http://'+url;
+        }
+        return url;
+      }
+    }
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  }
 });
 
+
+// virtual 함수는 에로우펑션(=>) 안먹힘!
+UserSchema.virtual('idpass').get(function(){
+  return this.userId+' '+this.password;
+});
+
+UserSchema.set('toObject', {getters: true, virtuals: true});
+UserSchema.set('toJSON', {getters: true, virtuals: true});
 mongoose.model('User', UserSchema);
